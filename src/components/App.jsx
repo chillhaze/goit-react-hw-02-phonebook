2ed77f36959e972d1f20ff5ae7e1b5f9aeca8819
chillhaze <alexander.gotra@gmail.com>
2021-09-07 01:07:20 +0300

    added firls to start

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// import { Counter } from './Counter';
+// import todosData from './data/todos.json';
+
+export class App extends Component {
+  state = {
+    todos: 1,
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
